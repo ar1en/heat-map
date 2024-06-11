0277f1c9a962c884ecae9babfa51813e1fc0c8d4
ar1en <ar1en@hotmail.com>
2024-06-11 17:26:45 +0300

    Placemarks

--- node-app/public/js/map.js
+++ node-app/public/js/map.js
@@ -16,21 +16,22 @@ function init() {
                 });
 
                 var heatmap = new Heatmap(heatmapData, {
-                    radius: 20,
+                    radius: 12,
                     opacity: 0.8,
                     intensityOfMidpoint: 0.2,
                     dissipating: true,
                     gradient: {
-                        0.1: 'rgba(37, 228, 49, 0.8)',
-                        0.2: 'rgba(235, 220, 12, 0.8)',
-                        0.3: 'rgba(255, 131, 6, 0.8)',
-                        0.4: 'rgba(162, 36, 25, 0.8)',
-                        1.0: 'rgba(136,32,23,0.89)'
+                        0.025: 'rgba(136,32,23,0.89)',
+                        0.1: 'rgba(255, 131, 6, 0.8)',
+                        0.3: 'rgba(235, 220, 12, 0.8)',
+                        0.4: 'rgba(37, 228, 49, 0.8)',
+                        1.0: 'rgba(37, 228, 49, 0.8)'
                     }
                 });
 
                 heatmap.setMap(myMap);
 
+                var allPlacemarks = [];
                 data.forEach(function (item) {
                     var coords = [item.geoData.coordinates[1], item.geoData.coordinates[0]];
                     var myPlacemark = new ymaps.Placemark(coords, {
@@ -39,9 +40,32 @@ function init() {
                         preset: 'islands#icon',
                         iconColor: '#0095b6'
                     });
-
-                    //myMap.geoObjects.add(myPlacemark);
+                    allPlacemarks.push(myPlacemark);
                 });
+
+                function updatePlacemarks() {
+                    var bounds = myMap.getBounds();
+                    var zoom = myMap.getZoom();
+                    var zoomThreshold = 15; // Пороговый уровень зума для отображения меток
+
+                    myMap.geoObjects.removeAll();
+
+                    if (zoom >= zoomThreshold) {
+                        allPlacemarks.forEach(function (placemark) {
+                            var coords = placemark.geometry.getCoordinates();
+                            if (coords[0] >= bounds[0][0] && coords[0] <= bounds[1][0] &&
+                                coords[1] >= bounds[0][1] && coords[1] <= bounds[1][1]) {
+                                myMap.geoObjects.add(placemark);
+                            }
+                        });
+                    }
+
+                    // Тепловая карта
+                    heatmap.setMap(myMap);
+                }
+
+                myMap.events.add('boundschange', updatePlacemarks);
+                updatePlacemarks(); // Обновляем метки при начальной загрузке
             });
         })
         .catch(error => console.error('Ошибка загрузки данных:', error));
