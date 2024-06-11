141081588e9968175770eeb183c9b5ae8328f829
Max Ivantsov <lp_kr@vk.com>
2024-06-12 02:44:53 +0300

    final

--- node-app/public/js/mail.js
+++ node-app/public/js/mail.js
@@ -6,13 +6,13 @@ const mailSender = (name, email, message) =>{
         message: message,
     };
 
-    emailjs.send('service_f88mcdk', 'template_i7njmdt', templateParams).then(
+    emailjs.send('service_he9o6ui', 'template_udxmz7r', templateParams).then(
         (response) => {
             alert("Спасибо! Ваша заявка принята в работу.")
             window.location.href = "conact_to_me.html";
         },
         (error) => {
-            alert("Ошибка отправки попробуйте еще")
+            alert("Ошибка отправки попробуйте еще!")
         },
     );
 
